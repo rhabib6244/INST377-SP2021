97ed4150b0a0d6434d773cbe3cad7d6e3d3def3c
Rida Habib <rhabib6244@gmail.com>
2021-02-12 21:30:09 -0500

    javascript

--- public/labs/lab_3/script.js
+++ public/labs/lab_3/script.js
@@ -1,14 +1,15 @@
 /* Put your javascript in here */
 
-function arrayMethod(){
-    const array1 = []
+function arrayMethod() {
+  const array1 = document.querySelector('#images');
 
-    const listContainer = document.createElement('ul');
-    const target = document.querySelector('#photos');
-    target.append(listContainer);
+  const listContainer = document.createElement('ul');
+  const target = document.querySelector('.ribbon');
+  target.append(listContainer);
 
-        array1.
-
-
-    
+  array1.forEach((element) => {
+    const listItem = document.createElement('li');
+    listItem = element;
+    listContainer.append(listItem);
+  });
 }
