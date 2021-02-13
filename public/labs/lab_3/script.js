6fe1cdc781b4da3e936f711ef05aca61bf6037d1
Rida Habib <rhabib6244@gmail.com>
2021-02-12 20:37:11 -0500

    m

--- public/labs/lab_3/script.js
+++ public/labs/lab_3/script.js
@@ -1,2 +1,14 @@
 /* Put your javascript in here */
 
+function arrayMethod(){
+    const array1 = []
+
+    const listContainer = document.createElement('ul');
+    const target = document.querySelector('#photos');
+    target.append(listContainer);
+
+        array1.
+
+
+    
+}
