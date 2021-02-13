6bf8353afabe2d569f9035eb8a740b2c947af880
Rida Habib <rhabib6244@gmail.com>
2021-02-12 22:58:46 -0500

    Update script.js

--- public/labs/lab_3/script.js
+++ public/labs/lab_3/script.js
@@ -1,21 +1,20 @@
 /* Put your javascript in here */
 
 function arrayMethod() {
-  const slideArray = [document.querySelector('#images')];
+  const slideArray = [document.querySelector(".li")];
 
-  const listContainer = document.createElement('ul');
-  const target = document.querySelector('.ribbon');
+  const listContainer = document.createElement("ul");
+  const target = document.querySelector(".carousel");
   target.append(listContainer);
 
-  array1.forEach((element) => {
-    const listItem = document.createElement('li');
-    listItem = element;
+  slideArray.forEach((element) => {
+    const listItem = document.createElement("li");
+    listItem.innerText = element;
     listContainer.append(listItem);
   });
 }
-
-function loadPage(){
-    document.addEventListener('click', (event) => {transform: translateX()
-
-    }
+function loadPage() {
+  document.addEventListener("click", (event) => {
+    arrayMethod();
+  });
 }
