59e07c7d65acbd23b6145d9710fe40e4943a17d2
Rida Habib <rhabib6244@gmail.com>
2021-02-13 21:23:25 -0500

    changes

--- public/labs/lab_3/script.js
+++ public/labs/lab_3/script.js
@@ -1,19 +1,40 @@
 /* Put your javascript in here */
 
-function arrayMethod() {
-  const slides = document.querySelectorAll("li");
-  const slideArray = Array.from(slides);
+ let i = 1;
 
-  const listContainer = document.createElement("ul");
-  const target = document.querySelector("#carou");
-  target.append(listContainer);
-}
-  slideArray.forEach((element) => {
-    const listItem = document.createElement("li");
-    listItem.innerText = element;
-    listContainer.append(listItem);
-    console.log(element);
-    button.addEventListener("click",(event) => {transform: translateX()
-  });
-}
-    button.addEventListener("click",(event) => alert("Hello!"));
+ let arrayOfImages = Array.from(carousel.querySelectorAll('li'));
+ window.console.log("arrOfImages:", arrayOfImages); 
+
+
+ arrayOfImages.forEach(li => {
+    li.style.position = 'relative';
+    li.insertAdjacentHTML('beforeend', `<span style="position:absolute;left:0;top:0">${i}</span>`);
+ });
+
+
+
+ /* configuration */
+ let width = 130; // image width
+ let count = 3; // visible images count
+
+ let list = carousel.querySelector('ul');
+ let listElems = carousel.querySelectorAll('li');
+
+ let position = 0; // ribbon scroll position
+
+function prevClicked(){
+   // shift left
+   position += width * count;
+   // can't move to the left too much, end of images
+   position = Math.min(position, 0)
+   list.style.marginLeft = position + 'px';
+ }
+
+ 
+ function nextClicked(){
+   // shift right
+   position -= width * count;
+   // can only shift the ribbbon for (total ribbon length - visible count) images
+   position = Math.max(position, -width * (listElems.length - count));
+   list.style.marginLeft = position + 'px';
+ }
