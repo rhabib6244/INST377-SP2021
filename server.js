4475600f227b3c81e7a1f4ea328706e8d764a555
Rida Habib <rhabib6244@gmail.com>
2021-02-19 20:34:08 -0500

    flappy bird!

--- server.js
+++ server.js
@@ -43,9 +43,10 @@ app.route('/api')
     console.log('POST request detected');
     console.log('Form data in res.body', req.body);
     console.log('Now send something back to your client');
+    res.send("Hello World");
     // res.json({data: dataToSendToFrontEnd});
   });
-
+  
 app.listen(port, async () => {
   console.log(`Example app listening on port ${port}!`);
 });
